3135ae222275a6cbaee06422f74e54abfc0a8505
Ruben Glapa <ruben.glapa@icloud.com>
2021-08-12 08:53:15 -0700

    Initial Commit

---
+++ Pet Watcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pet Watcher
+//
+//  Created by Ruben Glapa on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pet Watcher/Pet_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_WatcherApp.swift
+//  Pet Watcher
+//
+//  Created by Ruben Glapa on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Pet_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
